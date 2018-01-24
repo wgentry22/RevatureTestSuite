24c4876c5c08798b5c7a37ac3e72c1bad8493720
RobinPierre <robin.pierre16@outlook.com>
2018-01-24 15:08:16 -0500

    added a waitforload method on the MethodUtil class to which waits for elements to load for about a second

---
+++ src/main/java/com/revature/tester/MethodUtil.java
@@ -0,0 +1,34 @@
+package com.revature.tester;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MethodUtil {
+
+	int times;
+	public WebElement waitForLoad(WebDriver driver, String xpath) {
+		WebElement element = null;
+		while(times < 10 && element == null) {
+			try {
+				element = driver.findElement(By.xpath(xpath));
+			} catch(NoSuchElementException e) {
+				if(times < 10) {
+					try {
+						Thread.sleep(100);
+						times++;
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+				}
+				else {
+					e.printStackTrace();
+				}
+			}
+		}
+		return element;
+	}
+}
